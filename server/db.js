666510955b3e8bd0282e3e42c0174b266b59cad5
Gustavo Cacau <guga_cacau@hotmail.com>
2022-08-27 14:59:02 -0300

    Initial Server Commit

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const db = require("mysql2");
+
+const pool = db.createPool({
+  host: "database-pib.cp9unjk7ypjw.sa-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "admin",
+  password: "userunix123qwe",
+  database: "teste",
+});
+
+exports.pool = pool;
